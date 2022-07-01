35204eb59075fbfb15c7918784ef99b90e62e5f5
Seiya Nuta <nuta@seiya.me>
2022-07-01 17:53:01 +0900

    Update

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server"
+
+function generateCsrfToken(): string {
+    const bytes = crypto.getRandomValues(new Uint8Array(256 / 8));
+    return Array.from(bytes).map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+export default async function middleware(request: NextRequest) {
+    const response = NextResponse.next();
+    
+    console.log(request.nextUrl.pathname, request.method)
+    if (request.nextUrl.pathname.startsWith('/api') && !['GET', 'HEAD'].includes(request.method)) {
+        const tokenInCookie = request.cookies.get('csrf-token');
+        const tokenInRequest = request.headers.get('x-csrf-token');
+        if (tokenInCookie !== tokenInRequest) {
+            return new NextResponse(null, { status: 400 });
+        }
+    }
+
+    response.cookies.set('csrf-token', generateCsrfToken(), {
+        sameSite: 'strict',
+        httpOnly: false,
+        expires: new Date(new Date().getTime() + (3600 * 1000)),
+    });
+
+    return response;
+}
